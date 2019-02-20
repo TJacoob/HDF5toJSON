950bd1b6372bca5f8b21a873db02390e5a45b631
Tomás Jacob <tomas.j.martins@tecnico.ulisboa.pt>
2019-02-20 00:11:24 +0000

    Update
    - Added Time Of execution
    - Implemented rounding of values and coordinates

--- Examples2.py
+++ Examples2.py
@@ -1,10 +1,16 @@
 # HDF5 to JSON Converter
 
 import h5py
-import json
+#import json
+from decimal import Decimal
+import simplejson as json
+
+import time
 
 def main():
     #h5py.run_tests();
+    start = time.time()
+    print(start);
 
     geojs = {
         "type": "FeatureCollection",
@@ -21,9 +27,9 @@ def main():
 
     results = f['Results'];
 
-    for x in range(0,115):      # Should match the grid size
-        for y in range(0,115):
-            print(x,y);
+    for x in range(0,243):      # Should match the grid size
+        for y in range(0,114):
+            #print(x,y);
             unit = {
                 "type": "Feature",
                 "properties": {},
@@ -35,13 +41,15 @@ def main():
 
             # Grid
             coordinates = [
-                [longitude[x][y], latitude[x][y]],
-                [longitude[x+1][y+1], latitude[x][y]],
-                [longitude[x+1][y+1], latitude[x+1][y+1]],
-                [longitude[x][y], latitude[x+1][y+1]],
-                [longitude[x][y], latitude[x][y]],
+                [json.dumps(round(longitude[x][y],5)), json.dumps(round(latitude[x][y],5))],
+                [json.dumps(round(longitude[x+1][y+1],5)), json.dumps(round(latitude[x][y],5))],
+                [json.dumps(round(longitude[x+1][y+1],5)), json.dumps(round(latitude[x+1][y+1],5))],
+                [json.dumps(round(longitude[x][y],5)), json.dumps(round(latitude[x+1][y+1],5))],
+                [json.dumps(round(longitude[x][y],5)), json.dumps(round(latitude[x][y],5))],
             ];
             unit['geometry']['coordinates'][0]=coordinates;
+
+            #print(coordinates);
 
             # Annex Results
             for result in results:
@@ -50,9 +58,12 @@ def main():
                 #unit['properties'][stat.name[9:]]=0;
                 #print(stat);
                 for t in stat:
-                    if ( t[-5:] == "00001"):    # Only reading the first time entry
-                        #print(stat[t][0][x][y]);
-                        unit['properties'][stat.name[9:]] = stat[t][0][x][y];
+                    #print(stat[t]);
+                    if ( t[-5:] == "00001"):    # Only reading the first time entry (checks last 5 digits of the name)
+                        #print(round(Decimal(stat[t][0][x][y]),5));
+                        #unit['properties'][stat.name[9:]] = round(Decimal(stat[t][0][x][y]),5);   # Copies the value of the stat for this coordinate
+                        #unit['properties'][stat.name[9:]] = stat[t][0][x][y];
+                        unit['properties'][stat.name[9:]] = json.dumps(round(Decimal(stat[t][0][x][y]), 5));
 
             # Unit is Ready, annexing to the geojs
             geojs['features'].append(unit);
@@ -63,84 +74,11 @@ def main():
     with open('testFiles/newdata.json', 'w') as outfile:
         json.dump(geojs, outfile)
 
-    # print(jsonData);
+    #print(geojs);
 
     print("Exiting Program")
 
-
-
-
-    '''
-    grid = [];
-    for x in latitude:
-        yArray = [];
-        for y in x:
-            #print(y);
-            yArray.append(y);
-        grid.append(yArray);
-    #print(grid);
-
-    
-
-    # Time Information Handling
-    dates = f['Time'];
-    allDates = [];
-    for date in dates:
-        current = f['Time'][date];
-        #Should add an ISO 8601 format option
-        time = {
-            "year": int(current[0]),
-            "month": int(current[1]),
-            "day": int(current[2]),
-            "hour": int(current[3]),
-            "minute": int(current[4]),
-            "second": int(current[5])
-        }
-        allDates.append(time);
-
-
-    # Results Tables Handling
-    results = f['Results'];
-    print(results);
-    resultsData = {};
-    for result in results :
-        stat = f['Results'][result];
-        print(stat.name);
-
-        for t in stat:
-            col = stat[t];
-            #print(col);
-            r = [];
-
-            for x in col[0]:
-                yArray = [];
-                for y in x:
-                    yArray.append(y);
-                r.append(yArray);
-        #print(r);
-        resultsData[stat.name]=r;
-    #print(resultsData);
-
-
-
-
-
-    # JSON Object To Be Written
-    print("Exporting Data to JSON");
-    jsonData = {
-        'Grid':grid,
-        "Time":allDates,
-        "Data":resultsData
-    };
-
-    with open('testFiles/data.json', 'w') as outfile:
-        json.dump(jsonData, outfile)
-
-    #print(jsonData);
-
-    print("Exiting Program")
-    
-    '''
-
+    end = time.time()
+    print(end - start)
 
 main()
